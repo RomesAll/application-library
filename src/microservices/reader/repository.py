278dc890864a1bf206e5b14fb7fba1ebc70eaca8
RomesAll <romesky845@gmail.com>
2025-12-19 11:38:40 +0300

    добавил data access object для reader

---
+++ src/microservices/reader/repository.py
@@ -0,0 +1,46 @@
+from src.config import session_factory_async
+from src.config.models.readers import Readers
+from sqlalchemy import select
+import asyncio
+
+class ReaderRepository:
+
+    def __init__(self):
+        pass
+
+    async def select_all_reader_async(self):
+        async with session_factory_async() as session:
+            query = select(Readers)
+            records = await session.execute(query)
+            return records.scalars().all()
+
+    async def select_reader_by_id_async(self, reader_id: int):
+        async with session_factory_async() as session:
+            record = await session.get(Readers, {'id': reader_id})
+            return record
+
+    async def create_reader_async(self, orm_model: Readers):
+        async with session_factory_async() as session:
+            session.add(orm_model)
+            await session.flush()
+            await session.commit()
+            return orm_model
+
+    async def update_reader_async(self, orm_model: Readers):
+        async with session_factory_async() as session:
+            updating_orm_model = await session.get(Readers, {'id': orm_model.id})
+            if updating_orm_model is None:
+                pass
+            for k,v in orm_model.get_model_attributes().items():
+                if v is not None:
+                    setattr(updating_orm_model, k, v)
+            await session.commit()
+
+    async def delete_reader_async(self, reader_id: int):
+        async with session_factory_async() as session:
+            deleting_orm_model = await session.get(Readers, {'id': reader_id})
+            if deleting_orm_model is None:
+                pass
+            await session.delete(deleting_orm_model)
+            await session.commit()
+            return deleting_orm_model
---
+++ src/microservices/reader/services.py
@@ -0,0 +1,20 @@
+
+class ReaderService:
+
+    def __init__(self):
+        pass
+
+    async def select_all_reader_async(self):
+        pass
+
+    async def select_reader_by_id_async(self, reader_id: int):
+        pass
+
+    async def create_reader_async(self):
+        pass
+
+    async def update_reader_async(self):
+        pass
+
+    async def delete_reader_async(self):
+        pass
