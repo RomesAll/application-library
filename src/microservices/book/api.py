8371f9bc21fca29bf2f10f6b955a686a977b8ea3
RomesAll <romesky845@gmail.com>
2025-12-22 09:55:32 +0300

    добавил api для crud операций

--- src/microservices/book/api.py
+++ src/microservices/book/api.py
@@ -1,36 +1,36 @@
 from fastapi import APIRouter, Request, middleware
-from .schemas import ReaderCreateDTO, ReaderGetDTO, ReaderUpdateDTO, ReaderDeleteDTO
-from .services import ReaderService
+from .schemas import *
+from .services import *
 from .depends import db_session, pagination_params_dep
 from src.config import settings
 
-router = APIRouter(tags=['Reader'], prefix='/reader')
+router = APIRouter(tags=['Book'], prefix='/book')
 
-@router.get('/', description='Вывод всех читателей')
-async def get_all_readers(request: Request, session: db_session, pagination_params: pagination_params_dep) -> list[ReaderGetDTO]:
-    result = await ReaderService(db_session=session, request=request).select_all_reader_async(pagination_params)
+@router.get('/', description='Вывод всех книг')
+async def get_all_book(request: Request, session: db_session, pagination_params: pagination_params_dep) -> list[BookGetDTO]:
+    result = await BookService(db_session=session, request=request).select_all_book_async(pagination_params)
     return result
 
 
-@router.get('/{reader_id}', description='Вывод читателя по id')
-async def get_reader_by_id(reader_id: int, request: Request, session: db_session):
-    result = await ReaderService(db_session=session, request=request).select_reader_by_id_async(reader_id)
+@router.get('/{book_id}', description='Вывод книги по id')
+async def get_book_by_id(book_id: int, request: Request, session: db_session):
+    result = await BookService(db_session=session, request=request).select_book_by_id_async(book_id)
     return result
 
 
-@router.post('/', description='Добавить читателя')
-async def create_reader(reader: ReaderCreateDTO, request: Request, session: db_session):
-    result = await ReaderService(db_session=session, request=request).create_reader_async(reader)
+@router.post('/', description='Добавить книгу')
+async def create_reader(book: BookCreateDTO, request: Request, session: db_session):
+    result = await BookService(db_session=session, request=request).create_book_async(book)
     return result
 
 
-@router.put('/{reader_id}', description='Обновить читателя')
-async def update_reader_by_id(reader: ReaderUpdateDTO, request: Request, session: db_session):
-    result = await ReaderService(db_session=session, request=request).update_reader_async(reader)
+@router.put('/{book_id}', description='Обновить книгу')
+async def update_reader_by_id(book: BookUpdateDTO, request: Request, session: db_session):
+    result = await BookService(db_session=session, request=request).update_book_async(book)
     return result
 
 
-@router.delete('/{reader_id}', description='Удалить читателя по id')
-async def delete_reader_by_id(reader_id, request: Request, session: db_session) -> ReaderDeleteDTO:
-    result = await ReaderService(db_session=session, request=request).delete_reader_async(reader_id)
+@router.delete('/{book_id}', description='Удалить книгу по id')
+async def delete_reader_by_id(book_id, request: Request, session: db_session) -> BookDeleteDTO:
+    result = await BookService(db_session=session, request=request).delete_book_async(book_id)
     return result
